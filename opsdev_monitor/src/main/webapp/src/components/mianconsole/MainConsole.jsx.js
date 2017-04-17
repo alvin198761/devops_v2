9ebc238200f094525de120274f6b1b78883c0972
月球王子 <alvin198761@163.com>
2017-04-17 16:36:07 +0800

    修改监控的布局方式，更新到antd的高版本

---
+++ opsdev_monitor/src/main/webapp/src/components/mianconsole/MainConsole.jsx.js
@@ -0,0 +1,25 @@
+/**
+ * Created by tangzhichao on 2017/4/17.
+ */
+import React from 'react';
+import {Tabs,Button} from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+const MainConsole = (props) => {
+  return (
+    <Tabs
+      hideAdd
+      type="editable-card"
+      tabBarExtraContent={<Button>自定义按钮</Button>}>
+      <TabPane tab="主控制台" key="1">主控制台</TabPane>
+      <TabPane tab="常用实例1" key="2">常用实例1</TabPane>
+      <TabPane tab="常用实例2" key="3">常用实例2</TabPane>
+    </Tabs>
+  );
+};
+
+MainConsole.propTypes = {
+};
+
+export default MainConsole;
--- opsdev_monitor/src/main/webapp/src/router.js
+++ opsdev_monitor/src/main/webapp/src/router.js
@@ -1,9 +1,10 @@
 import React, {PropTypes} from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route,IndexRoute, browserHistory} from 'react-router';
 import {RouterStore, syncHistoryWithStore} from 'mobx-react-router';
 
 import IndexPage from './routes/IndexPage';
-import Overview from './components/Overview';
+// import Overview from './components/Overview';
+import MainConsole from './components/mianconsole/MainConsole.jsx';
 import List from './components/List';
 import LineChartDemo from './components/LineChartDemo';
 export const routingStore = new RouterStore();
@@ -13,7 +14,8 @@ const history = syncHistoryWithStore(browserHistory, routingStore);
 export const CustomRouter = () => (
   <Router history={history}>
     <Route path='/' component={IndexPage}>
-      <Route path='/overview' component={Overview}/>
+      <IndexRoute components={MainConsole}></IndexRoute>
+      <Route path='/overview' component={MainConsole}/>
       <Route path='/list' component={List}/>
       <Route path='/chart' component={LineChartDemo}/>
     </Route>
